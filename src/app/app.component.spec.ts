a10fec91f654f3a853d4279ea0aea79aa8bad2fd
Joanh <joanh_gomez@hotmail.com>
2019-01-25 16:20:06 +1300

    t

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'Peeps'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('jg-app');
+    expect(app.title).toEqual('Peeps');
   });
 
   it('should render title in a h1 tag', () => {
