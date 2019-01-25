322f279a4b5464760d68a17c2fed882136c6ce66
Joanh <joanh_gomez@hotmail.com>
2019-01-25 16:17:09 +1300

    test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'jg-app'`, () => {
+  it(`should have as title 'Peeps'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('jg-app');
